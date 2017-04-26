fb3dca7c10ee85a24f3f9b43fa0a3b297f49acb1
Wallace Maxters <wallacemaxters@gmail.com>
2017-04-26 13:22:43 -0300

    removendo arquivo não mais necessário

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once __DIR__ . '/../vendor/autoload.php';
-
-include_once __DIR__ . '/../vendor/laravel/laravel/tests/TestCase.php';
-
-include_once __DIR__ . '/ValidatorTestCase.php';
