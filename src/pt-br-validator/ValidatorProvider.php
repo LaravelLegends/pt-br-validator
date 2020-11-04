c6238ccd169c479873b3ae6046ac76be0bd2257f
WallaceMaxters <wallace.vizerra@donadvice.com>
2020-11-04 16:09:44 -0300

    atualizando mensagens de validação com ponto e adicionando mensagens de validação de telefone e celular com código

--- src/pt-br-validator/ValidatorProvider.php
+++ src/pt-br-validator/ValidatorProvider.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace LaravelLegends\PtBrValidator;
 
@@ -14,22 +14,20 @@ class ValidatorProvider extends ServiceProvider
      */
     protected $defer = false;
 
-    
+
     /**
      * Bootstrap the application events.
      *
      * @return void
      */
-     
+
     public function boot()
     {
 
-        $me = $this;
-
-        $this->app['validator']->resolver(function ($translator, $data, $rules, $messages, $customAttributes) use($me)
+        $this->app['validator']->resolver(function ($translator, $data, $rules, $messages, $customAttributes)
         {
-            $messages += $me->getMessages();
-            
+            $messages += $this->getMessages();
+
             return new Validator($translator, $data, $rules, $messages, $customAttributes);
         });
     }
@@ -37,19 +35,22 @@ class ValidatorProvider extends ServiceProvider
 
     protected function getMessages()
     {
-        return [        
-            'celular'          => 'O campo :attribute não é um celular válido',
-            'celular_com_ddd'  => 'O campo :attribute não é um celular com DDD válido',
-            'cnh'              => 'O campo :attribute não é uma carteira nacional de habilitação válida',
-            'cnpj'             => 'O campo :attribute não é um CNPJ válido',
-            'cpf'              => 'O campo :attribute não é um CPF válido',
-            'data'             => 'O campo :attribute não é uma data com formato válido',
-            'formato_cnpj'     => 'O campo :attribute não possui o formato válido de CNPJ',
-            'formato_cpf'      => 'O campo :attribute não possui o formato válido de CPF',
-            'telefone'         => 'O campo :attribute não é um telefone válido',
-            'telefone_com_ddd' => 'O campo :attribute não é um telefone com DDD válido',
-            'formato_cep'      => 'O campo :attribute não possui um formato válido de CEP',
-            'formato_placa_de_veiculo'   => 'O campo :attribute não possui um formato válido de placa',
+        return [
+            'celular'                  => 'O campo :attribute não é um celular válido.',
+            'celular_com_ddd'          => 'O campo :attribute não é um celular com DDD válido.',
+            'celular_com_codigo'       => 'O campo :attribute não é um celular com código e DDD válido.',
+            'cnh'                      => 'O campo :attribute não é uma carteira nacional de habilitação válida.',
+            'cnpj'                     => 'O campo :attribute não é um CNPJ válido.',
+            'cpf'                      => 'O campo :attribute não é um CPF válido.',
+            'data'                     => 'O campo :attribute não é uma data com formato válido.',
+            'formato_cnpj'             => 'O campo :attribute não possui o formato válido de CNPJ.',
+            'formato_cpf'              => 'O campo :attribute não possui o formato válido de CPF.',
+            'telefone'                 => 'O campo :attribute não é um telefone válido.',
+            'telefone_com_ddd'         => 'O campo :attribute não é um telefone com DDD válido.',
+            'telefone_com_codigo'      => 'O campo :attribute não é um telefone com código e DDD válido.',
+            'formato_cep'              => 'O campo :attribute não possui um formato válido de CEP.',
+            'formato_placa_de_veiculo' => 'O campo :attribute não possui um formato válido de placa.',
+
         ];
     }
 
